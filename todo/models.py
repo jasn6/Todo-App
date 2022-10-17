1969b2b3e9e4d07a2c563fe60fd20166f2434a01
jasn6 <110363877+jasn6@users.noreply.github.com>
2022-10-16 23:01:08 -0400

    v2

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Todo(models.Model):
-    name = models.TextField()
+    todo = models.TextField()
 
     def __str__(self):
         return self.name
